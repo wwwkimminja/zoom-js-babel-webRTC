a22132e370469d6a29f48d7edd298f9c8fa0bd24
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-07-03 18:05:01 +0900

    WebSocket Event: connect

--- src/server.js
+++ src/server.js
@@ -17,4 +17,9 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
+function handleConnection(socket) {
+  console.log(socket);
+}
+wss.on("connection", handleConnection);
+
 server.listen(3000, handleListen);
