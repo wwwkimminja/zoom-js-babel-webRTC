daff2d9efbacc63dd7b94559a7a04566a31f989e
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-07-03 17:13:57 +0900

    setup: front

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import express from "express";
-
+import path from 'path';
+const __dirname = path.resolve();
 const app = express();
-console.log("hello");
 
-app.listen(3000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/public/views");
+app.use("/public", express.static(__dirname + "/src/public"));
+app.get("/", (req, res) => res.render("home"));
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
