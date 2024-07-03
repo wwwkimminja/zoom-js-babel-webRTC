f397aec067b3d74720fe9313815625e35ec36816
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-07-03 17:47:47 +0900

    WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 import express from "express";
 import path from 'path';
+import http from "http";
+import WebSocket from "ws";
 const __dirname = path.resolve();
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/src/public/views");
 app.use("/public", express.static(__dirname + "/src/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
