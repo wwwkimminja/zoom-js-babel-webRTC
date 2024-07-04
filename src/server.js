36d087ec11629703dea522f88892af183781f566
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-07-04 15:00:40 +0900

    feat : chat

--- src/server.js
+++ src/server.js
@@ -17,11 +17,15 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = []
+
 wss.on("connection", (socket) => {
+  sockets.push(socket)
   console.log("Connected to Browser ✅")
   socket.on("close", () => console.log("Disconnected from the Browser ❌"))
-  socket.on("message", (message) => console.log(message.toString()))
-  socket.send("hello!")
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()))
+  })
 
 });
 
